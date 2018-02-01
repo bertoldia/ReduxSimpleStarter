c2114d69f07ba04ce778f5f5b0d721c256f69066
Axel von Bertoldi <bertoldia@gmail.com>
2018-02-01 16:39:53 -0700

    Section 2, Lecture 31

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+
+  onInputChange(event) {
+    const { target: { value }} = event;
+    this.setState({ term: value });
+    this.props.onSearchTermChange(value);
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+      </div>
+    );
+  }
+}
+export default SearchBar;
