d008b877a7a3ff1ca5af1e998e99f911b3063173
Axel von Bertoldi <bertoldia@gmail.com>
2018-02-06 16:40:29 -0700

    Section 5, Lesson 58

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = "92ccfc5057045190c4eba1203bd7e885";
+import axios from 'axios';
+
+const API_KEY = '92ccfc5057045190c4eba1203bd7e885';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
