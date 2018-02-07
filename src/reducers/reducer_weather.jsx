3bb21bbec7d3404cbc5b10025759c11d16e5960a
Axel von Bertoldi <bertoldia@gmail.com>
2018-02-07 10:45:05 -0700

    Section 5, Lesson 60

--- src/reducers/reducer_weather.jsx
+++ src/reducers/reducer_weather.jsx
@@ -1,5 +1,12 @@
 // @format
 
-export default function(state = null, action) {
-  return state;
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
+    default:
+      return state;
+  }
 }
