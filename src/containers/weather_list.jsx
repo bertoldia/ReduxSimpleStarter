106dc8c3c0178bfe00bf98c41aa5ccda78daeebd
Axel von Bertoldi <abertoldi@atb.com>
2018-02-07 11:46:59 -0700

    Section 5, Lesson 64

--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -2,13 +2,33 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const { city: { name } } = cityData;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const press = cityData.list.map(weather => weather.main.pressure);
+    const humid = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={press}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={humid}>
+            <SparklinesLine color="green" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
